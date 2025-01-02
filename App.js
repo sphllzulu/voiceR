ae8f28ab9ace06b16d4d5c0def2b55c120ff7d92
Siphelele <sphllzulu@gmail.com>
2025-01-02 16:06:42 +0200

    added expo splash screen

--- App.js
+++ App.js
@@ -1,11 +1,53 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, ActivityIndicator } from 'react-native';
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from 'expo-font';
+import { useEffect, useState } from 'react';
 import AudioRecording from './components/voiceRecording';
 
+
+SplashScreen.preventAutoHideAsync();
+
 export default function App() {
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        // Load fonts or other resources
+        await Font.loadAsync({
+          'Outfit': require('./assets/fonts/Outfit-VariableFont_wght.ttf'),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  // Hide the splash screen when the app is ready
+  useEffect(() => {
+    if (appIsReady) {
+      SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  // Show a loading indicator while waiting for the app to be ready
+  if (!appIsReady) {
+    return (
+      <View style={styles.loadingContainer}>
+        <ActivityIndicator size="large" color="#ffffff" />
+      </View>
+    );
+  }
+
   return (
     <View style={styles.container}>
-      <AudioRecording/>
+      <AudioRecording />
       <StatusBar style="auto" />
     </View>
   );
@@ -13,6 +55,12 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,
+    backgroundColor: '#000000',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  loadingContainer: {
     flex: 1,
     backgroundColor: '#000000',
     alignItems: 'center',
